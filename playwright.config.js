b956f33c00db0f794174e7f8f0c1fb90b764e2af
VladRoman99 <vlad.roman@toptal.com>
2024-11-11 19:12:43 +0200

    Trigger workflow rerun

--- playwright.config.js
+++ playwright.config.js
@@ -2,11 +2,11 @@
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
-    testDir: './src/tests', // Update this path if your tests are located elsewhere
-    timeout: 30000, // Set a timeout for tests (30 seconds)
-    retries: 1, // Set number of retries for failed tests
+    testDir: './src/tests',
+    timeout: 30000,
+    retries: 1,
     use: {
-        headless: true, // Run tests in headless mode in CI
+        headless: true,
     },
-    reporter: 'html', // Generate an HTML report for Playwright tests
+    reporter: 'html',
 });
